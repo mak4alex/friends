c2f0241ba4cc1d91f2e110b00b86be8f1da1fe52
Alexander Maksimovich <maksimovich.a@profitero.com>
2017-10-09 21:49:06 +0300

    add job optimization

--- app/jobs/online_status_update_job.rb
+++ app/jobs/online_status_update_job.rb
@@ -2,29 +2,27 @@ class OnlineStatusUpdateJob < ApplicationJob
 
   before_enqueue do |job|
     @@statuses = Set.new
+    @@user_ids = User.select(:vk_user_id).map(&:vk_user_id)
   end
 
   def perform(*args)
-    statuses = VK::Client.new.friends.get(
-      user_id: ENV['VK_SPYER_ID'],
-      fields:  User::ONLINE_FIELDS
+    statuses = VK::Client.new.users.get(
+      user_ids: @@user_ids,
+      fields:   User::ONLINE_FIELDS
     ).to_set
 
     new_statuses = statuses - @@statuses
-
-    if !@@statuses.empty?
-      new_statuses.each do |user_data|
-        next if user_data["deactivated"] # "banned"
-        OnlineStatus.create({
-          :vk_user_id => user_data['user_id'],
-          :last_seen  => Time.at(user_data['last_seen']['time']),
-          :platform   => user_data['last_seen']['platform'],
-          :online     => user_data['online'] == User::ONLINE,
-          :online_mobile => user_data.key?('online_mobile')
-        })
-      end
+    method = @@statuses.empty? ? :find_or_create_by : :create
+    new_statuses.each do |user_data|
+      next if user_data["deactivated"] # "banned"
+      OnlineStatus.send(method, {
+        :vk_user_id => user_data['uid'],
+        :last_seen  => Time.at(user_data['last_seen']['time']),
+        :platform   => user_data['last_seen']['platform'],
+        :online     => user_data['online'] == User::ONLINE,
+        :online_mobile => user_data.key?('online_mobile')
+      })
     end
-
     @@statuses = statuses
   end
 end
