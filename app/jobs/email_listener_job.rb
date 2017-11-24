977a1b831a612a4982f2de94aac71adf1e500278
Alexander Maksimovich <maksimovich.a@profitero.com>
2017-11-25 01:28:43 +0300

    Change email sending to sync mode

--- app/jobs/email_listener_job.rb
+++ app/jobs/email_listener_job.rb
@@ -5,7 +5,7 @@ class EmailListenerJob < ApplicationJob
       from_email = email.from.first
       task.log.info("Got new email #{subject} from #{from_email}")
       if email.subject.gsub(/\W/, '') =~ /givemeyouripbaby/i
-        SupportMailer.send_public_id(from_email).deliver_later
+        SupportMailer.send_public_id(from_email).deliver_now
       end      
     end
   end
